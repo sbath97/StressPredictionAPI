d4258d59fdbc44780afd7eb60fe37481d6700528
sbath97 <111360599+sbath97@users.noreply.github.com>
2024-08-27 12:58:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import joblib
 
 
 
-#model = pickle.load(open('svm_model.pkl','rb'))
 model = joblib.load('svm_model.pkl')
 #model = joblib.load('RandomForest_model.pkl')
 
@@ -24,19 +23,10 @@ def predict():
     humidity = request.form.get ('humidity')
     temperature = request.form.get('temperature')
     step_count = request.form.get('step_count')
-    #stress_level = request.form.get('stress_level')
     respiratery_rate = request.form.get('respiratery_rate')
     heart_rate = request.form.get('heart_rate')
     input_query = np.array([[humidity, temperature, step_count, respiratery_rate, heart_rate]])
-    print(input_query)
-    #input_query = [[humidity, temperature, step_count, respiratery_rate, heart_rate]]
-    #df1 = pd.DataFrame(input_query)
     result = model.predict(input_query)
-
-  
-    #result = {'humidity':humidity, 'temperature':temperature, 'step_count':step_count, 'stress_level':stress_level, 'respiratery_rate':respiratery_rate, 'heart_rate':heart_rate}
-    print(result[0])
-    #return jsonify(str(result))
     return jsonify({'stress_level':str(result)})
 
 
